1044b9aacf53cffca018b0d4a1d704dce121bf69
Name <kattie@outlook.com>
2023-12-18 10:27:46 +0100

    1,2,3 -ready

--- 3.js
+++ 3.js
@@ -1,7 +1,6 @@
-/* Написать функцию, которая создает пустой объект, но без прототипа. */
 function createObj () {
 	let obj = Object.create(null);
 	return obj;
 }
 const myObject = createObj();
-alert(Object.getPrototypeOf(myObject) === null);
+alert(Object.getPrototypeOf(myObject) === null); // проверка пустого obj
