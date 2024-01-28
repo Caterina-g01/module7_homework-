ce4d279e8314eb3c522a9a0ca639c1ab6587ea7c
Name <kattie@outlook.com>
2024-01-28 10:47:11 +0100

    2, 4 are corrected

--- 2.js
+++ 2.js
@@ -1,4 +1,25 @@
-function checkAvailabilitty(str, obj) {
+const isProp = (string, obj) => {
+	return string in obj
+}
+
+const myObject = {
+	name: "Ivan",
+	city: "Moscow",
+	age: 25
+}
+
+console.log(isProp("city", myObject))
+
+
+
+
+
+
+
+
+
+/*
+function isProp(str, obj) {
 	for (let key in obj) {
 		if (obj[key] === str) {
 			return true;
@@ -16,9 +37,12 @@ const user = {
 }
 
 const myString = "Ivan";
+*/
 
 
-console.log(checkAvailabilitty(myString, user));
+
+
+
 
 
 
--- 4.js
+++ 4.js
@@ -1,3 +1,100 @@
+/* Исправленная версия */
+
+function Device(name, brand, power) {
+	this.name = name;
+	this.brand = brand;
+	this.power = power;
+	this.type = "electric device";
+	this.pluggedIn = false;
+}
+
+let totalPower = 0;
+
+Device.prototype.turnOn = function () {
+	if (!this.pluggedIn) {
+		this.pluggedIn = true;
+		console.log(`${this.name} is now plugged in.`);
+		this.calcPower(this);
+	} else {
+		console.log(`${this.name} is already plugged in.`);
+	}
+};
+
+Device.prototype.turnOff = function () {
+	if (this.pluggedIn) {
+		this.pluggedIn = false;
+		console.log(`${this.name} is now unplugged.`);
+		totalPower -= this.power;
+	} else {
+		console.log(`${this.name} is already unplugged.`);
+	}
+};
+
+
+Device.prototype.calcPower = function (device) {  // передала аргумент ф-и подсчёта
+	if (device.pluggedIn === true) {
+		totalPower += device.power;
+	}
+};
+
+
+function LapTop(brand, power, brightness) {
+	Device.call(this, "Laptop", brand, power);
+	this.brightness = brightness;
+}
+
+LapTop.prototype = Object.create(Device.prototype);
+LapTop.prototype.constructor = LapTop;
+
+LapTop.prototype.changeBrightness = function (percent) {
+	console.log(`brightness on this ${this.name} is ${percent}%.`);
+};
+
+function TV(brand, power, screenSize) {
+	Device.call(this, "TV", brand, power);
+	this.screenSize = screenSize;
+}
+
+TV.prototype = Object.create(Device.prototype);
+TV.prototype.constructor = TV;
+
+TV.prototype.changeChannel = function (channel) {
+	console.log(`${this.name} is now tuned to channel ${channel}.`);
+};
+
+function GameConsole(brand, power, gameCount) {
+	Device.call(this, "Game Console", brand, power);
+	this.gameCount = gameCount;
+}
+
+GameConsole.prototype = Object.create(Device.prototype);
+GameConsole.prototype.constructor = GameConsole;
+
+GameConsole.prototype.playGame = function (game) {
+	console.log(`Playing ${game} on ${this.name}.`);
+};
+
+const lapTop = new LapTop("Acer", 120);
+const tv = new TV("Samsung", 130);
+const gameConsole = new GameConsole("Nintendo", 160);
+
+
+tv.changeChannel(3);
+
+lapTop.changeBrightness(100);
+
+gameConsole.turnOn();
+gameConsole.playGame("Super Mario");
+
+
+tv.turnOn();
+
+console.log(totalPower);
+
+
+
+/* Предыдущая версия дз 
+
 function Device(name, brand, power) {
 	this.name = name;
 	this.brand = brand;
@@ -89,9 +186,9 @@ console.log(totalPower);
 
 
 
-// Альтернативный способ с помощью массива 
 
-/*
+
+
 function Device(name, brand, power) {
 	this.name = name;
 	this.brand = brand;
